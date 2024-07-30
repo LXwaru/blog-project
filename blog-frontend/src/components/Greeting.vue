fb4c83eb0669595a82fdadb14256c96517adf787
Alexander Rubin <your@email.here>
2024-07-30 17:04:45 -0500

    create nonfucntional login page

---
+++ blog-frontend/src/components/Greeting.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            account: false
+        }
+    }
+}
+</script>
+
+<template>
+    <div> 
+        <h1 v-if="account">Welcome {{ username }}</h1>
+        <h1 v-else>Sign up or log in</h1>
+        <p v-if="account">here are your blogs!</p>
+        <p v-else>here are some other people's blogs</p>
+    </div>
+</template>
---
+++ blog-frontend/src/components/Login.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+export default {
+    data() {
+        return {
+            message: "Login",
+            username: "username", 
+            password: "password",
+            createAccount: "create account",
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h2>{{message}}</h2>
+        <br>
+        <input placeholder="username" /><br>
+        <input placeholder="password" /><br>
+        <button>{{ message }}</button><br>
+        <button>{{ createAccount }}</button>
+    </div>
+</template>
+
+
