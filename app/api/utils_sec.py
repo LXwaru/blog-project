57cba081e91510fade069a92aceea0da58efc281
Alexander Rubin <your@email.here>
2024-08-02 11:25:05 -0500

    update routing for user list

--- app/api/utils_sec.py
+++ app/api/utils_sec.py
@@ -12,7 +12,8 @@ import os
 
 load_dotenv()
 
-
+# to get a string like this run:
+# openssl rand -hex 32
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
--- blog-frontend/src/router/index.ts
+++ blog-frontend/src/router/index.ts
@@ -10,12 +10,20 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/ListUsers',
+      name: 'ListUsers',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/ListUsers.vue')
+    },
+    {
+      path: '/listBlogsByUser',
+      name: 'listBlogsByUser',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ListBlogsByUser.vue')
     }
   ]
 })
