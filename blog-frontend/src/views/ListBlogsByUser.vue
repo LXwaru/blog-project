bd4fdeb7f77c642009c8db3a5d28716ca1176403
Alexander Rubin <your@email.here>
2024-08-04 23:00:35 -0500

    fix username in listBlogsByUser view

--- blog-frontend/src/views/ListBlogsByUser.vue
+++ blog-frontend/src/views/ListBlogsByUser.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>{{userId}}'s blogs</h1>
+        <h1>{{user.username}}'s blogs</h1>
         <p  v-for="item in items" :key="item.id">
             <RouterLink :to="{ name: 'DetailBlog', params: { itemId: item.id } }">{{ item.title }}</RouterLink>    
         </p>
@@ -23,7 +23,8 @@ export default {
     },
     data() {
         return {
-            items: []
+            items: [],
+            user: {}
 
         }
     },
@@ -35,10 +36,17 @@ export default {
         let itemResults = await axios.get(`http://localhost:8000/api/users/${this.userId}/items/`, {
             params: {
                 owner_id: userId
+            },
+        })
+        let userResults = await axios.get(`http://localhost:8000/api/user/${this.userId}`, {
+            params: {
+                id: userId
             }
         })
             console.log(itemResults.data);
+            console.log(userResults.data);
             this.items = itemResults.data;
+            this.user = userResults.data;
     }
 }
 </script>
